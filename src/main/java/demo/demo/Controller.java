7fe343acff3af3967e68fc677dbf55a032fa34a9
zablon <zablonoigo51@gmail.com>
2026-09-01 13:26:30 +0300

    app controller

---
+++ src/main/java/demo/demo/Controller.java
@@ -0,0 +1,13 @@
+package demo.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller{
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
